d5d110323b3f59ae7cdc7cb2cf8299337dabb4a2
Gregor Dietrich <takku@live.de>
2019-12-30 20:31:28 +0100

    gitignore fix re

--- src/System/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-// required in /app/src/Core/Container.php function __construct()
-$_ENV['dbhost'] = "localhost";
-$_ENV['dbname'] = "";
-$_ENV['dbuser'] = "";
-$_ENV['dbpass'] = "";
-$_ENV['dbchar'] = "utf8";
-
-?>
