e48122f4b9581a9436b230c78bebecdcc4f97edd
Gregor Dietrich <takku@live.de>
2019-12-30 20:15:57 +0100

    db cleanup

--- src/System/db.php
+++ src/System/db.php
@@ -1,9 +1,9 @@
 <?php
 // required in /app/src/Core/Container.php function __construct()
 $_ENV['dbhost'] = "localhost";
-$_ENV['dbname'] = "blog";
-$_ENV['dbuser'] = "blog";
-$_ENV['dbpass'] = "K3O?EcWv!dxSJrT7";
+$_ENV['dbname'] = "";
+$_ENV['dbuser'] = "";
+$_ENV['dbpass'] = "";
 $_ENV['dbchar'] = "utf8";
 
 ?>
