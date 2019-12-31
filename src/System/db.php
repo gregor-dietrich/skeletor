28593478e224ba45c06f01430a8bed0b634c04a1
Gregor Dietrich <takku@live.de>
2019-12-31 11:04:18 +0100

    tester

---
+++ src/System/db.php
@@ -0,0 +1,8 @@
+<?php
+// required in /app/src/Core/Container.php function __construct()
+$_ENV['dbhost'] = "localhost";
+$_ENV['dbname'] = "";
+$_ENV['dbuser'] = "";
+$_ENV['dbpass'] = "";
+$_ENV['dbchar'] = "utf8";
+?>
