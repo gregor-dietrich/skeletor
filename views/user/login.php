08833ecaeb7ca071986abb51bd5278c679194b07
Gregor Dietrich <takku@live.de>
2020-01-01 11:39:10 +0100

    login form updated

--- views/user/login.php
+++ views/user/login.php
@@ -1,37 +1,29 @@
 <?php include __DIR__ . "/../layout/header.php"; ?>
-    <br/>
-
-<?php if (!empty($error)): ?>
-    <p class="alert-danger">
-        <?php echo $error; ?>
-    </p>
-<?php endif; ?>
-    <div class="row">
-        <div class="col-md-12">
-            <h3>Login</h3>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
+                <div class="card card-signin my-5">
+                    <?php if (!empty($error)): ?>
+                        <p class="alert-danger">
+                            <?php echo $error; ?>
+                        </p>
+                    <?php endif; ?>
+                    <div class="card-body">
+                        <h5 class="card-title text-center">Sign In</h5>
+                        <form method="POST" action="login" class="form-signin">
+                            <div class="form-label-group">
+                                <input type="text" id="username" name="username" class="form-control" placeholder="Username" required autofocus>
+                                <label for="username">Username</label>
+                            </div>
+                            <div class="form-label-group">
+                                <input type="password" id="password" name="password" class="form-control" placeholder="Password" required>
+                                <label for="password">Password</label>
+                            </div>
+                            <button class="btn btn-lg btn-primary btn-block text-uppercase" type="submit" style="border-radius:20px;">Sign in</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
-        <form method="POST" action="login" class="form-horizontal">
-            <div class="form-group">
-                <label class="control-label col-md-3" for="username">
-                    Username
-                </label>
-                <div class="col-md-9">
-                    <input type="text" name="username" id="username" class="form-control" required/>
-                </div>
-            </div>
-            <div class="form-group">
-                <label class="control-label col-md-3" for="password">
-                    Password
-                </label>
-                <div class="col-md-9">
-                    <input type="password" name="password" id="password" class="form-control" required/>
-                </div>
-            </div>
-            <div class="form-group">
-                <div class="col-md-12">
-                    <input type="submit" name="login" value="Login" id="login" class="btn btn-primary"/>
-                </div>
-            </div>
-        </form>
     </div>
 <?php include __DIR__ . "/../layout/footer.php"; ?>
