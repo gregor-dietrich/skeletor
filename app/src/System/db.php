953addab217e28f1d6202a0d378d6d3b0ab21908
Gregor Dietrich <takku@live.de>
2020-01-01 12:18:42 +0100

    more cleanup

--- app/src/System/db.php
+++
@@ -1,8 +0,0 @@
-<?php
-// required in /app/src/Core/Container.php function __construct()
-$_ENV['dbhost'] = "localhost";
-$_ENV['dbname'] = "blog";
-$_ENV['dbuser'] = "blog";
-$_ENV['dbpass'] = "K3O?EcWv!dxSJrT7";
-$_ENV['dbchar'] = "utf8";
-?>
