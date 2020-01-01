9d13ff101619c023a0f6b21e8e7134e086fb6b7c
Gregor Dietrich <takku@live.de>
2020-01-01 08:12:33 +0100

    changing titles

--- views/layout/admin/header.php
+++ views/layout/admin/header.php
@@ -31,11 +31,11 @@
     <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
         <!-- Sidebar - Brand -->
-        <a class="sidebar-brand d-flex align-items-center justify-content-center" href="/app/index.php/dashboard">
+        <a class="sidebar-brand d-flex align-items-center justify-content-center" href="/">
             <div class="sidebar-brand-icon rotate-n-15">
-                <i class="fas fa-tachometer-alt"></i>
+                <i class="fas fa-smile-wink"></i>
             </div>
-            <div class="sidebar-brand-text mx-3">Dashboard</div>
+            <div class="sidebar-brand-text mx-3">Project Title</div>
         </a>
 
         <!-- Divider -->
--- views/layout/header.php
+++ views/layout/header.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
     <meta name="description" content=""/>
     <meta name="author" content=""/>
-    <title>Project Name Title</title>
+    <title>Project Title</title>
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
           rel="stylesheet" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
           crossorigin="anonymous"/>
@@ -14,7 +14,7 @@
 <body>
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top">
     <div class="container">
-        <a class="navbar-brand" href="/">Project Name Title</a>
+        <a class="navbar-brand" href="/">Project Title</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive"
                 aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
