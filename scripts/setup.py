03c83f73d0100d0124e09d7e7471fba61007c62b
Gregor Dietrich <takku@live.de>
2020-01-07 22:36:12 +0100

    moved some files, updated setup script

--- scripts/setup.py
+++ scripts/setup.py
@@ -1,31 +1,127 @@
+
+import mysql.connector
 import os
 import shutil
+import sys
 
-errors = 0
-files = {
-    "db.php": "../app/src/System/",
-    "salt.php": "../app/src/System/"
-}
 root = os.path.dirname(__file__)
 
-for filename, subdir in files.items():
-    subdirs = subdir.split("/")[1:]
-    pathing = "../"
-    for dir in subdirs:
-        pathing += dir + "/"
-        if not os.path.exists(os.path.join(root, pathing)):
-            os.mkdir(os.path.join(root, pathing))
 
-    loc = os.path.join(root,  subdir, filename)
-    if not os.path.isfile(loc):
-        print("Couldn't find " + filename + ". Creating...")
-        if os.path.isfile(loc[:-3] + "default.php"):
-            shutil.copyfile(loc[:-3] + "default.php", loc)
-        else:
-            print("Error: " + filename[:-3] + "default.php not found.")
-            errors += 1
+def init_db():
+    sql_file = os.path.join(root + "/setup/app.sql")
+
+    if os.path.isfile(sql_file):
+        auth = {
+            "db_host": input("Hostname: "),
+            "db_name": input("Database: "),
+            "db_user": input("Username: "),
+            "db_pass": input("Password: ")
+        }
+
+        try:
+            print("Connecting to database...")
+            connection = mysql.connector.connect(
+                user=auth["db_user"],
+                password=auth["db_pass"],
+                host=auth["db_host"],
+                database=auth["db_name"])
+            if connection.is_connected():
+                print("Connected to database '%s'!" % auth["db_name"])
+                cursor = connection.cursor()
+
+                write_auth = input("Save credentials to db.php? (y/n) ")
+                if write_auth == "y":
+                    print("Writing credentials to db.php...")
+                    # TO DO
+                    print("Finished writing to db.php.")
+                else:
+                    print("Skipped writing to db.php.")
+
+                drop_tables = input("Drop database tables (if they exist)? (y/n) ")
+                if drop_tables == "y":
+                    print("Dropping database tables...")
+                    cursor.execute("DROP TABLE IF EXISTS `post_comments`, `posts`, `post_categories`;")
+                    cursor.execute("DROP TABLE IF EXISTS `users`, `user_ranks`;")
+                    print("Database tables dropped.")
+                else:
+                    print("Not dropping existing tables (if any).")
+
+                create_tables = input("Create database tables? (y/n) ")
+                if create_tables == "y":
+                    print("Creating database tables...")
+                    parse_sql(sql_file, cursor)
+                else:
+                    print("Database initialization skipped.")
+
+        except mysql.connector.Error as e:
+            if "Query was empty" not in str(e):
+                print("Database connection error: ", e)
+            else:
+                print("Finished parsing %s." % sql_file)
+
+        finally:
+            if "connection" in locals():
+                if connection.is_connected():
+                    print("Closing database connection...")
+                    cursor.close()
+                    connection.close()
+                    print("Connection closed.")
+
     else:
-        print(filename + " already exists. Skipping...")
+        print("Error: " + root + "/setup/app.sql not found.")
 
-print("\n%d Errors encountered during setup." % errors)
-input("Press any key to exit... ")
+
+def init_files():
+    files = {
+        "db.php": "../app/src/System/",
+        "salt.php": "../app/src/System/"
+    }
+
+    for filename, subdir in files.items():
+        sub_dirs = subdir.split("/")
+        make_dir = sub_dirs[0] + "/"
+        for dir_name in sub_dirs[1:]:
+            make_dir += dir_name + "/"
+            if not os.path.exists(os.path.join(root, make_dir)):
+                os.mkdir(os.path.join(root, make_dir))
+
+        target = os.path.join(root,  subdir, filename)
+        if not os.path.isfile(target):
+            print("Couldn't find " + filename + ". Creating...")
+            src = os.path.join(root + "/setup/" + filename[:-3] + "default.php")
+            if os.path.isfile(src):
+                shutil.copyfile(src, target)
+                print("Success.")
+            else:
+                print("Error: " + root + "/setup/" + filename[:-3] + "default.php not found.")
+        else:
+            print(filename + " already exists. Skipping...")
+
+
+def parse_sql(sql_file, cursor):
+    file = open(sql_file, "r", encoding="utf-8")
+    contents = file.read()
+    file.close()
+    commands = contents.split(";")
+    for command in commands:
+        cursor.execute(command)
+
+
+def main():
+    create_files = input("Create default files? (y/n) ")
+    if create_files == "y":
+        init_files()
+    else:
+        print("Default file creation skipped.")
+
+    create_connection = input("Create MySQL connection? (y/n) ")
+    if create_connection == "y":
+        init_db()
+    else:
+        print("MySQL connection skipped.")
+
+    print("All operations completed. Exiting...")
+    sys.exit()
+
+
+main()
