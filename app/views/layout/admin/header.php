71970ce1d3dc770e462b080b878d9943c069d619
Gregor Dietrich <takku@live.de>
2020-01-15 20:53:30 +0100

    Fix dashboard

--- app/views/layout/admin/header.php
+++ app/views/layout/admin/header.php
@@ -352,22 +352,18 @@
                         <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                             <span class="mr-2 d-none d-lg-inline text-gray-600 small">
-                                <?php echo $_SESSION['login']; ?>
+                                Howdy, <?php echo $_SESSION['login']; ?>!
                             </span>
                             <img class="img-profile rounded-circle" src="https://cataas.com/cat?type=sq" alt=""/>
                         </a>
                         <!-- Dropdown - User Information -->
                         <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
                              aria-labelledby="userDropdown">
-                            <a class="dropdown-item" href="/app/index.php/user?id=<?php
-                            echo $this->usersRepository->findUsername($_SESSION['login'])->id;
-                            ?>">
+                            <a class="dropdown-item" href="#">
                                 <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
                                 Profile
                             </a>
-                            <a class="dropdown-item" href="/app/index.php/dashboard/users/edit?id=<?php
-                            echo $this->usersRepository->findUsername($_SESSION['login'])->id;
-                            ?>">
+                            <a class="dropdown-item" href="#">
                                 <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
                                 Settings
                             </a>
