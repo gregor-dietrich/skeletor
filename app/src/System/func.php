0b037eb5b8937ceee05c3cd6179340ce3fca2a4c
Gregor Dietrich <takku@live.de>
2020-01-15 23:58:12 +0100

    add functions to split datetime

--- app/src/System/func.php
+++ app/src/System/func.php
@@ -5,6 +5,19 @@ function datetime_now()
     return date('Y-m-d H:i:s', time());
 }
 
+function datetime_date($string)
+{
+    $date = explode("-", explode(" ", $string)[0]);
+    return implode(".", array_reverse($date));
+}
+
+function datetime_time($string)
+{
+    $time = explode(":", explode(" ", $string)[1]);
+    array_pop($time);
+    return implode(":", $time);
+}
+
 function escape($string)
 {
     return htmlentities($string, ENT_QUOTES, 'UTF-8');
