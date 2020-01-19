e9d56f7e391786598c998305039ef8112032bb75
Gregor Dietrich <takku@live.de>
2020-01-20 00:00:11 +0100

    Update generate data script (wip)

--- scripts/generate_data.wip.py
+++ scripts/generate_data.wip.py
@@ -14,6 +14,17 @@ def check_folders(subdir):
             os.mkdir(os.path.join(make_dir))
 
 
+def get_input(table):
+    user_input = int(input("Insert how many rows into table %s? " % table))
+    if user_input < 0:
+        user_input = abs(user_input)
+    return user_input
+
+
+def lorem_ipsum():
+    return ""
+
+
 def random_string(length, mode="letters"):
     result = ""
     alphabet = string.ascii_letters
@@ -24,6 +35,11 @@ def random_string(length, mode="letters"):
         result += "$2y$10$"
         length -= 7
         alphabet += "/."
+    elif mode == "hex":
+        del alphabet
+        alphabet = [str(i) for i in range(0, 10)]
+        alphabet = "".join(alphabet)
+        alphabet += "abcdef"
 
     random.shuffle(alphabet.split())
     for i in range(length):
@@ -63,6 +79,10 @@ def write_sql(module, query, charset):
 def main():
     modules = ["user_groups", "users", "user_groups_meta", "post_categories", "posts", "post_comments"]
     queries = []
+    user_groups = 0
+    users = 0
+    post_categories = 0
+    posts = 0
 
     for module in modules:
         query = "INSERT INTO `" + module + "` ("
@@ -82,33 +102,89 @@ def main():
         query += ") VALUES "
 
         if module == "user_groups":
+            user_groups = get_input("user_groups")
             group_types = ["Corporation", "Association", "Team", "Group", "Syndicate", "Squad", "Partnership"]
-            query += "(\"" + random_string(random.randint(3, 10)).capitalize()
-            if random.randint(0, 1) == 1:
-                query += " " + random_string(random.randint(3, 10)).capitalize()
-            query += " " + group_types[random.randint(0, len(group_types) - 1)]
-            query += "\")"
+            for i in range(user_groups):
+                query += "(\"" + random_string(random.randint(3, 10)).capitalize()
+                if random.randint(0, 1) == 1:
+                    query += " " + random_string(random.randint(3, 10)).capitalize()
+                query += " " + group_types[random.randint(0, len(group_types) - 1)]
+                query += "\")"
+                if i != user_groups - 1:
+                    query += ","
         elif module == "users":
-            query += "(\"" + random_string(random.randint(3, 12)).capitalize() + "\",\""\
-                     + random_string(60, mode="hash") + "\",\""\
-                     + random_string(20, mode="alphanumeric").lower() + "\",\"1\",\"" + random_email()\
-                     + "\",\"1\",\"::1\",\""\
-                     + "2019-" + random_date(1, 12) + "-" + random_date(1, 28) + " " + random_date(0, 23)\
-                     + ":" + random_date(0, 59) + ":" + random_date(0, 59) + "\",\""\
-                     + "2020-01-" + random_date(1, 31) + " " + random_date(0, 23)\
-                     + ":" + random_date(0, 59) + ":" + random_date(0, 59) + "\")"
+            users = get_input("users")
+            for i in range(users):
+                query += "(\"" + random_string(random.randint(3, 12)).capitalize() + "\",\""\
+                         + random_string(60, mode="hash") + "\",\""\
+                         + random_string(20, mode="hex").lower() + "\",1,\"" + random_email()\
+                         + "\",1,\"::1\",\""\
+                         + "2019-" + random_date(1, 12) + "-" + random_date(1, 28) + " " + random_date(0, 23)\
+                         + ":" + random_date(0, 59) + ":" + random_date(0, 59) + "\",\""\
+                         + "2020-01-" + random_date(20, 31) + " " + random_date(0, 23)\
+                         + ":" + random_date(0, 59) + ":" + random_date(0, 59) + "\")"
+                if i != users - 1:
+                    query += ","
         elif module == "user_groups_meta":
-            query += ""
+            user_groups_meta = get_input("user_groups_meta")
+            pairs = []
+            while len(pairs) < user_groups_meta:
+                user_id, group_id = str(random.randint(2, users+1)), str(random.randint(1, user_groups))
+                pair = user_id + "," + group_id
+                if pair not in pairs:
+                    pairs.append(pair)
+                    query += "("
+                    query += user_id + "," + group_id + ",\""
+                    query += "2020-01-" + random_date(1, 19) + " " + random_date(0, 23)\
+                             + ":" + random_date(0, 59) + ":" + random_date(0, 59) + "\")"
+                    if len(pairs) != user_groups_meta:
+                        query += ","
         elif module == "post_categories":
-            query += ""
+            post_categories = get_input("post_categories")
+            parents = ["General", "Other", "Technology", "Programming", "Webdesign", "Science", "Politics", "Economy"]
+            for i in range(post_categories):
+                if i < len(parents):
+                    query += "(\"" + parents[i] + "\",NULL"
+                else:
+                    if i == len(parents):
+                        query += ";\n" + "INSERT INTO `" + module + "` (`name`,`parent_id`) VALUES "
+                    query += "(\""
+                    query += random_string(random.randint(3, 9)).capitalize()
+                    for j in range(random.randint(1, 3)):
+                        query += " " + random_string(random.randint(2, 12)).capitalize()
+                    query += "\"," + str(random.randint(1, len(parents)))
+                query += ")"
+                if i != post_categories - 1 and i != len(parents) - 1:
+                    query += ","
         elif module == "posts":
-            query += ""
+            posts = get_input("posts")
+            for i in range(posts):
+                query += "(\""
+                query += random_string(random.randint(3, 9)).capitalize()
+                for j in range(random.randint(2, 4)):
+                    query += " " + random_string(random.randint(2, 12)).capitalize()
+                query += "\",\"" + lorem_ipsum() + "\","
+                user_id, category_id = str(random.randint(2, users+1)), str(random.randint(1, post_categories))
+                query += "%s,%s,1,1,\"" % (user_id, category_id)
+                query += "2020-01-" + random_date(1, 14) + " " + random_date(0, 23)\
+                         + ":" + random_date(0, 59) + ":" + random_date(0, 59) + "\",\""
+                query += "2020-01-" + random_date(15, 19) + " " + random_date(0, 23)\
+                         + ":" + random_date(0, 59) + ":" + random_date(0, 59) + "\")"
+                if i != posts - 1:
+                    query += ","
         elif module == "post_comments":
-            query += ""
+            post_comments = get_input("post_comments")
+            for i in range(post_comments):
+                query += "(\"" + lorem_ipsum() + "\","
+                query += str(random.randint(1, posts)) + "," + str(random.randint(2, users+1))
+                query += ",\"" + "2020-01-" + random_date(15, 19) + " " + random_date(0, 23)\
+                         + ":" + random_date(0, 59) + ":" + random_date(0, 59) + "\")"
+                if i != post_comments - 1:
+                    query += ","
         query += ";"
 
         queries.append(query)
-        # write_sql(module, query, "utf-8")
+        write_sql(module, query, "utf-8")
 
     print("All operations completed. Exiting...")
     sys.exit()
