8925dd4c7042670136442a886b9252f4d8e2604d
Gregor Dietrich <takku@live.de>
2020-01-21 06:25:33 +0100

    Update SQL

--- scripts/setup/app.sql
+++ scripts/setup/app.sql
@@ -15,9 +15,9 @@ START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT = @@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS = @@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION = @@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
 --
@@ -30,11 +30,13 @@ SET time_zone = "+00:00";
 -- Table structure for table `pages`
 --
 
-CREATE TABLE `pages` (
-  `id` int(11) UNSIGNED NOT NULL,
-  `title` varchar(255) NOT NULL,
-  `content` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+CREATE TABLE `pages`
+(
+    `id`      int(11) UNSIGNED NOT NULL,
+    `title`   varchar(255)     NOT NULL,
+    `content` text             NOT NULL
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4;
 
 --
 -- RELATIONSHIPS FOR TABLE `pages`:
@@ -46,17 +48,20 @@ CREATE TABLE `pages` (
 -- Table structure for table `posts`
 --
 
-CREATE TABLE `posts` (
-  `id` int(11) UNSIGNED NOT NULL,
-  `title` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `content` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `user_id` int(11) DEFAULT NULL,
-  `category_id` int(11) DEFAULT NULL,
-  `published` tinyint(1) NOT NULL DEFAULT 0,
-  `commentable` tinyint(1) NOT NULL DEFAULT 0,
-  `created` datetime NOT NULL DEFAULT current_timestamp(),
-  `last_edit` datetime DEFAULT NULL ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+CREATE TABLE `posts`
+(
+    `id`          int(11) UNSIGNED                        NOT NULL,
+    `title`       varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `content`     text COLLATE utf8mb4_unicode_ci         NOT NULL,
+    `user_id`     int(11) UNSIGNED                        NULL,
+    `category_id` int(11) UNSIGNED                                 DEFAULT NULL,
+    `published`   tinyint(1)                              NOT NULL DEFAULT 0,
+    `commentable` tinyint(1)                              NOT NULL DEFAULT 0,
+    `created`     datetime                                NOT NULL DEFAULT current_timestamp(),
+    `last_edit`   datetime                                         DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_unicode_ci;
 
 --
 -- RELATIONSHIPS FOR TABLE `posts`:
@@ -72,11 +77,14 @@ CREATE TABLE `posts` (
 -- Table structure for table `post_categories`
 --
 
-CREATE TABLE `post_categories` (
-  `id` int(11) UNSIGNED NOT NULL,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `parent_id` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+CREATE TABLE `post_categories`
+(
+    `id`        int(11) UNSIGNED                        NOT NULL,
+    `name`      varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `parent_id` int(11) UNSIGNED DEFAULT NULL
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_unicode_ci;
 
 --
 -- RELATIONSHIPS FOR TABLE `post_categories`:
@@ -90,13 +98,16 @@ CREATE TABLE `post_categories` (
 -- Table structure for table `post_comments`
 --
 
-CREATE TABLE `post_comments` (
-  `id` int(11) UNSIGNED NOT NULL,
-  `content` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `post_id` int(11) NOT NULL,
-  `user_id` int(11) DEFAULT NULL,
-  `created` datetime NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+CREATE TABLE `post_comments`
+(
+    `id`      int(11) UNSIGNED                NOT NULL,
+    `content` text COLLATE utf8mb4_unicode_ci NOT NULL,
+    `post_id` int(11) UNSIGNED                NOT NULL,
+    `user_id` int(11) UNSIGNED                NULL,
+    `created` datetime                        NOT NULL DEFAULT current_timestamp()
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_unicode_ci;
 
 --
 -- RELATIONSHIPS FOR TABLE `post_comments`:
@@ -112,19 +123,22 @@ CREATE TABLE `post_comments` (
 -- Table structure for table `users`
 --
 
-CREATE TABLE `users` (
-  `id` int(11) UNSIGNED NOT NULL,
-  `username` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `salt` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `rank_id` int(11) DEFAULT NULL,
-  `email` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `activated` tinyint(1) NOT NULL DEFAULT 0,
-  `activation_key` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `last_ip` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `created` datetime NOT NULL DEFAULT current_timestamp(),
-  `last_login` datetime NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+CREATE TABLE `users`
+(
+    `id`             int(11) UNSIGNED                        NOT NULL,
+    `username`       varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `password`       varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `salt`           varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `rank_id`        int(11) UNSIGNED                                 DEFAULT NULL,
+    `email`          varchar(255) COLLATE utf8mb4_unicode_ci          DEFAULT NULL,
+    `activated`      tinyint(1)                              NOT NULL DEFAULT 0,
+    `activation_key` varchar(255) COLLATE utf8mb4_unicode_ci NULL,
+    `last_ip`        varchar(45) COLLATE utf8mb4_unicode_ci           DEFAULT NULL,
+    `created`        datetime                                NOT NULL DEFAULT current_timestamp(),
+    `last_login`     datetime                                NOT NULL DEFAULT current_timestamp()
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_unicode_ci;
 
 --
 -- RELATIONSHIPS FOR TABLE `users`:
@@ -136,8 +150,8 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `password`, `salt`, `rank_id`, `activated`) VALUES
-(1, 'admin', '$2y$10$CTxymd5XnDLBftintJ6BWOPTziaP.csS74zyiZwAo05tNEVgwxvIG', '364bf7426d7925814a43', 3, 1);
+INSERT INTO `users` (`id`, `username`, `password`, `salt`, `rank_id`, `activated`)
+VALUES (1, 'admin', '$2y$10$CTxymd5XnDLBftintJ6BWOPTziaP.csS74zyiZwAo05tNEVgwxvIG', '364bf7426d7925814a43', 3, 1);
 
 -- --------------------------------------------------------
 
@@ -145,10 +159,12 @@ INSERT INTO `users` (`id`, `username`, `password`, `salt`, `rank_id`, `activated
 -- Table structure for table `user_groups`
 --
 
-CREATE TABLE `user_groups` (
-  `id` int(11) UNSIGNED NOT NULL,
-  `name` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+CREATE TABLE `user_groups`
+(
+    `id`   int(11) UNSIGNED NOT NULL,
+    `name` varchar(255)     NOT NULL
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4;
 
 --
 -- RELATIONSHIPS FOR TABLE `user_groups`:
@@ -160,12 +176,14 @@ CREATE TABLE `user_groups` (
 -- Table structure for table `user_groups_meta`
 --
 
-CREATE TABLE `user_groups_meta` (
-  `id` int(11) UNSIGNED NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `group_id` int(11) NOT NULL,
-  `timestamp` datetime NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+CREATE TABLE `user_groups_meta`
+(
+    `id`        int(11) UNSIGNED NOT NULL,
+    `user_id`   int(11) UNSIGNED NOT NULL,
+    `group_id`  int(11) UNSIGNED NOT NULL,
+    `timestamp` datetime         NOT NULL DEFAULT current_timestamp()
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4;
 
 --
 -- RELATIONSHIPS FOR TABLE `user_groups_meta`:
@@ -181,28 +199,31 @@ CREATE TABLE `user_groups_meta` (
 -- Table structure for table `user_ranks`
 --
 
-CREATE TABLE `user_ranks` (
-  `id` int(11) UNSIGNED NOT NULL,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `post_add` tinyint(1) NOT NULL DEFAULT 0,
-  `post_delete` tinyint(1) NOT NULL DEFAULT 0,
-  `post_edit` tinyint(1) NOT NULL DEFAULT 0,
-  `post_category_add` tinyint(1) NOT NULL DEFAULT 0,
-  `post_category_delete` tinyint(1) NOT NULL DEFAULT 0,
-  `post_category_edit` tinyint(1) NOT NULL DEFAULT 0,
-  `post_comment_add` tinyint(1) NOT NULL DEFAULT 0,
-  `post_comment_delete` tinyint(1) NOT NULL DEFAULT 0,
-  `post_comment_edit` tinyint(1) NOT NULL DEFAULT 0,
-  `user_add` tinyint(1) NOT NULL DEFAULT 0,
-  `user_delete` tinyint(1) NOT NULL DEFAULT 0,
-  `user_edit` tinyint(1) NOT NULL DEFAULT 0,
-  `user_group_add` tinyint(1) NOT NULL DEFAULT 0,
-  `user_group_delete` tinyint(1) NOT NULL DEFAULT 0,
-  `user_group_edit` tinyint(1) NOT NULL DEFAULT 0,
-  `user_rank_add` tinyint(1) NOT NULL DEFAULT 0,
-  `user_rank_delete` tinyint(1) NOT NULL DEFAULT 0,
-  `user_rank_edit` tinyint(1) NOT NULL DEFAULT 0
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+CREATE TABLE `user_ranks`
+(
+    `id`                   int(11) UNSIGNED                        NOT NULL,
+    `name`                 varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `post_add`             tinyint(1)                              NOT NULL DEFAULT 0,
+    `post_delete`          tinyint(1)                              NOT NULL DEFAULT 0,
+    `post_edit`            tinyint(1)                              NOT NULL DEFAULT 0,
+    `post_category_add`    tinyint(1)                              NOT NULL DEFAULT 0,
+    `post_category_delete` tinyint(1)                              NOT NULL DEFAULT 0,
+    `post_category_edit`   tinyint(1)                              NOT NULL DEFAULT 0,
+    `post_comment_add`     tinyint(1)                              NOT NULL DEFAULT 0,
+    `post_comment_delete`  tinyint(1)                              NOT NULL DEFAULT 0,
+    `post_comment_edit`    tinyint(1)                              NOT NULL DEFAULT 0,
+    `user_add`             tinyint(1)                              NOT NULL DEFAULT 0,
+    `user_delete`          tinyint(1)                              NOT NULL DEFAULT 0,
+    `user_edit`            tinyint(1)                              NOT NULL DEFAULT 0,
+    `user_group_add`       tinyint(1)                              NOT NULL DEFAULT 0,
+    `user_group_delete`    tinyint(1)                              NOT NULL DEFAULT 0,
+    `user_group_edit`      tinyint(1)                              NOT NULL DEFAULT 0,
+    `user_rank_add`        tinyint(1)                              NOT NULL DEFAULT 0,
+    `user_rank_delete`     tinyint(1)                              NOT NULL DEFAULT 0,
+    `user_rank_edit`       tinyint(1)                              NOT NULL DEFAULT 0
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_unicode_ci;
 
 --
 -- RELATIONSHIPS FOR TABLE `user_ranks`:
@@ -212,10 +233,13 @@ CREATE TABLE `user_ranks` (
 -- Dumping data for table `user_ranks`
 --
 
-INSERT INTO `user_ranks` (`id`, `name`, `post_add`, `post_delete`, `post_edit`, `post_category_add`, `post_category_delete`, `post_category_edit`, `post_comment_add`, `post_comment_delete`, `post_comment_edit`, `user_add`, `user_delete`, `user_edit`, `user_group_add`, `user_group_delete`, `user_group_edit`, `user_rank_add`, `user_rank_delete`, `user_rank_edit`) VALUES
-(1, 'User', 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
-(2, 'Moderator', 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0),
-(3, 'Administrator', 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1);
+INSERT INTO `user_ranks` (`id`, `name`, `post_add`, `post_delete`, `post_edit`, `post_category_add`,
+                          `post_category_delete`, `post_category_edit`, `post_comment_add`, `post_comment_delete`,
+                          `post_comment_edit`, `user_add`, `user_delete`, `user_edit`, `user_group_add`,
+                          `user_group_delete`, `user_group_edit`, `user_rank_add`, `user_rank_delete`, `user_rank_edit`)
+VALUES (1, 'User', 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+       (2, 'Moderator', 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+       (3, 'Administrator', 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1);
 
 --
 -- Indexes for dumped tables
@@ -225,62 +249,65 @@ INSERT INTO `user_ranks` (`id`, `name`, `post_add`, `post_delete`, `post_edit`, 
 -- Indexes for table `pages`
 --
 ALTER TABLE `pages`
-  ADD PRIMARY KEY (`id`);
-ALTER TABLE `pages` ADD FULLTEXT KEY `content` (`content`);
+    ADD PRIMARY KEY (`id`);
+ALTER TABLE `pages`
+    ADD FULLTEXT KEY `content` (`content`);
 
 --
 -- Indexes for table `posts`
 --
 ALTER TABLE `posts`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `posts_ibfk_1` (`user_id`),
-  ADD KEY `posts_ibfk_2` (`category_id`);
-ALTER TABLE `posts` ADD FULLTEXT KEY `content` (`content`);
+    ADD PRIMARY KEY (`id`),
+    ADD KEY `posts_ibfk_1` (`user_id`),
+    ADD KEY `posts_ibfk_2` (`category_id`);
+ALTER TABLE `posts`
+    ADD FULLTEXT KEY `content` (`content`);
 
 --
 -- Indexes for table `post_categories`
 --
 ALTER TABLE `post_categories`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `post_categories_ibfk_1` (`parent_id`);
+    ADD PRIMARY KEY (`id`),
+    ADD KEY `post_categories_ibfk_1` (`parent_id`);
 
 --
 -- Indexes for table `post_comments`
 --
 ALTER TABLE `post_comments`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `post_comments_ibfk_1` (`user_id`),
-  ADD KEY `post_comments_ibfk_2` (`post_id`);
-ALTER TABLE `post_comments` ADD FULLTEXT KEY `content` (`content`);
+    ADD PRIMARY KEY (`id`),
+    ADD KEY `post_comments_ibfk_1` (`user_id`),
+    ADD KEY `post_comments_ibfk_2` (`post_id`);
+ALTER TABLE `post_comments`
+    ADD FULLTEXT KEY `content` (`content`);
 
 --
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `users_username` (`username`),
-  ADD UNIQUE KEY `users_email` (`email`),
-  ADD KEY `users_ibfk_1` (`rank_id`);
+    ADD PRIMARY KEY (`id`),
+    ADD UNIQUE KEY `users_username` (`username`),
+    ADD UNIQUE KEY `users_email` (`email`),
+    ADD KEY `users_ibfk_1` (`rank_id`);
 
 --
 -- Indexes for table `user_groups`
 --
 ALTER TABLE `user_groups`
-  ADD PRIMARY KEY (`id`);
+    ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `user_groups_meta`
 --
 ALTER TABLE `user_groups_meta`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `user_id` (`user_id`,`group_id`),
-  ADD KEY `group_id` (`group_id`);
+    ADD PRIMARY KEY (`id`),
+    ADD UNIQUE KEY `user_id` (`user_id`, `group_id`),
+    ADD KEY `group_id` (`group_id`);
 
 --
 -- Indexes for table `user_ranks`
 --
 ALTER TABLE `user_ranks`
-  ADD PRIMARY KEY (`id`);
+    ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -290,49 +317,49 @@ ALTER TABLE `user_ranks`
 -- AUTO_INCREMENT for table `pages`
 --
 ALTER TABLE `pages`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+    MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+    MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `post_categories`
 --
 ALTER TABLE `post_categories`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+    MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `post_comments`
 --
 ALTER TABLE `post_comments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+    MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+    MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `user_groups`
 --
 ALTER TABLE `user_groups`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+    MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `user_groups_meta`
 --
 ALTER TABLE `user_groups_meta`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+    MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `user_ranks`
 --
 ALTER TABLE `user_ranks`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+    MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- Constraints for dumped tables
@@ -342,36 +369,36 @@ ALTER TABLE `user_ranks`
 -- Constraints for table `posts`
 --
 ALTER TABLE `posts`
-  ADD CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
-  ADD CONSTRAINT `posts_ibfk_2` FOREIGN KEY (`category_id`) REFERENCES `post_categories` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+    ADD CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
+    ADD CONSTRAINT `posts_ibfk_2` FOREIGN KEY (`category_id`) REFERENCES `post_categories` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
 
 --
 -- Constraints for table `post_categories`
 --
 ALTER TABLE `post_categories`
-  ADD CONSTRAINT `post_categories_ibfk_1` FOREIGN KEY (`parent_id`) REFERENCES `post_categories` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+    ADD CONSTRAINT `post_categories_ibfk_1` FOREIGN KEY (`parent_id`) REFERENCES `post_categories` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
 
 --
 -- Constraints for table `post_comments`
 --
 ALTER TABLE `post_comments`
-  ADD CONSTRAINT `post_comments_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
-  ADD CONSTRAINT `post_comments_ibfk_2` FOREIGN KEY (`post_id`) REFERENCES `posts` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+    ADD CONSTRAINT `post_comments_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
+    ADD CONSTRAINT `post_comments_ibfk_2` FOREIGN KEY (`post_id`) REFERENCES `posts` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `users`
 --
 ALTER TABLE `users`
-  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`rank_id`) REFERENCES `user_ranks` (`id`) ON UPDATE CASCADE;
+    ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`rank_id`) REFERENCES `user_ranks` (`id`) ON UPDATE CASCADE;
 
 --
 -- Constraints for table `user_groups_meta`
 --
 ALTER TABLE `user_groups_meta`
-  ADD CONSTRAINT `user_groups_meta_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `user_groups_meta_ibfk_2` FOREIGN KEY (`group_id`) REFERENCES `user_groups` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+    ADD CONSTRAINT `user_groups_meta_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+    ADD CONSTRAINT `user_groups_meta_ibfk_2` FOREIGN KEY (`group_id`) REFERENCES `user_groups` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40101 SET CHARACTER_SET_CLIENT = @OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS = @OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION = @OLD_COLLATION_CONNECTION */;
